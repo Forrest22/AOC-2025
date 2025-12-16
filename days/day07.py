0cf5d8d652c749220ee01f6b72c53b85dd4e699c
Forrest22 <forforr@gmail.com>
2025-12-16 14:36:46 -0500

    Day 7 part 2 vast time improvement refactor

--- days/day07.py
+++ days/day07.py
@@ -89,7 +89,22 @@ def process_quantum_manifold_splits(tachyonManifold: List[str]) -> int:
 def solve_part2(inputData: str) -> int:
     """
     input_data: full contents of inputs/day07.txt as a string.
+    Inspired from https://old.reddit.com/r/adventofcode/comments/1pg9w66/2025_day_7_solutions/nsq17au/
+    because my recursive solution, while right was incredibly slow.
     return: (part 2 answer)
     """
-    tachyonManifold = parse_tachyon_manifold(inputData)
-    return process_quantum_manifold_splits(tachyonManifold)
+    splitters = [
+        col for line in inputData.splitlines() for col, x in enumerate(line) if x == "^"
+    ]
+    entering = [1] + [0 for _ in range(0, len(splitters) - 1)]
+
+    # for each splitter, compare with each other after it
+    for i, si in enumerate(splitters):
+        for j, sj in enumerate(splitters[:i][::-1]):
+            # if the columns are the same then they won't carry split beam
+            if si == sj:
+                break
+            # if the difference of columns is one, increase the beam count for that col
+            if abs(si - sj) == 1:
+                entering[i] += entering[i - j - 1]
+    return sum(entering) + 1
